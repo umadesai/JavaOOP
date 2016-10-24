a581db1636d174227cf0be5215a64bec449cd31b
Uma Desai <uma.desai@students.olin.edu>
2016-10-24 15:36:37 -0400

    submitting Exercise 01

---
+++ Cat.java
@@ -0,0 +1,9 @@
+class Cat {
+	int size;
+	String breed;
+	String name;
+
+	void bark() {
+		System.out.println("I'm a cat.");
+	}
+}
---
+++ DogTestDrive.java
@@ -0,0 +1,11 @@
+class DogTestDrive {
+	public static void main (String[] args) {
+		Dog Kevin = new Dog();
+		Kevin.size = 40;
+		Kevin.bark();
+
+		Cat Kelly = new Cat();
+		Kelly.breed = "Maine Coon";
+		Kelly.bark();
+	}
+}
