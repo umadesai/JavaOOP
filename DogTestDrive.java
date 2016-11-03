4848c91feaa083253c13ce6e8f5030fdc3d94de7
Uma Desai <uma.desai@students.olin.edu>
2016-11-03 15:56:33 -0400

    updating Exercise 3 Part 1: isBigger

--- DogTestDrive.java
+++ DogTestDrive.java
@@ -1,14 +1,13 @@
 /**
  * @author Uma Desai
- * DogTestDrive creates an array of dogs, invokes bark on each dog,
- * finds the biggest dog, and prints its instance variables.
+ * DogTestDrive tests the getSize, setSize, and isBigger methods of the Dog class.
  */
 
 class DogTestDrive {
 	public static void main (String[] args) {
 
 		Dog dog = new Dog();
-		dog.setSize(50);
+		dog.setSize(40);
 		Dog dog2 = new Dog();
 		dog2.setSize(60);
 		System.out.println(dog.isBigger(dog2));
@@ -16,36 +15,24 @@ class DogTestDrive {
 	}
 }
 
-// class DogTestDrive {
-// 	public static void main (String[] args) {
+class Dog {
+	private int size;
+	String breed;
+	String name;
 
-// 		Dog[] dogs;
-// 		dogs = new Dog[3];
-// 		dogs[0] = new Dog();
-// 		dogs[0].size = 60;
-// 		dogs[0].breed = "Great Dane";
-// 		dogs[0].name = "Dan";
-// 		dogs[1] = new Dog();
-// 		dogs[1].size = 40;
-// 		dogs[1].breed = "Labrador Retriever";
-// 		dogs[1].name = "Lilly";
-// 		dogs[2] = new Dog();
-// 		dogs[2].size = 10;
-// 		dogs[2].breed = "Chihuahua";
-// 		dogs[2].name = "Charlie";
+	public void setSize(int sz){
+		size = sz;
+	}
 
-// 		int x = 0;
-// 		int biggestDog = 0;
-// 		while (x < dogs.length) {
-// 			dogs[x].bark();
-// 			if (dogs[x].size > dogs[biggestDog].size) {
-// 				biggestDog = x;
-// 			}
-// 			if (x == 2) {
-// 				System.out.println("Biggest dog: " + dogs[biggestDog].name + ", " + dogs[biggestDog].breed + ", " + dogs[biggestDog].size);
-// 			}
-// 			x = x + 1;
-// 		}
+	public int getSize(){
+		return size;
+	}
 
-// 	}
-// }
+	public boolean isBigger(Dog d) {
+		return d.getSize() > size;
+	}
+
+	void bark() {
+		System.out.println("Ruff! Ruff!");
+	}
+}
