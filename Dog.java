92696d7d398bced9c981bfc6ecc376cf5874338b
Uma Desai <uma.desai@students.olin.edu>
2016-11-03 14:35:52 -0400

    updating with exercise 3

--- Dog.java
+++ Dog.java
@@ -1,7 +1,23 @@
 class Dog {
-	int size;
+	private int size;
 	String breed;
 	String name;
+
+	public void setSize(int sz){
+		size = sz;
+	}
+
+	public int getSize(){
+		return size;
+	}
+
+	public boolean isBigger(Dog d) {
+		if (d.getSize() > size) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	void bark() {
 		System.out.println("Ruff! Ruff!");
--- DogTestDrive.java
+++ DogTestDrive.java
@@ -7,33 +7,45 @@
 class DogTestDrive {
 	public static void main (String[] args) {
 
-		Dog[] dogs;
-		dogs = new Dog[3];
-		dogs[0] = new Dog();
-		dogs[0].size = 60;
-		dogs[0].breed = "Great Dane";
-		dogs[0].name = "Dan";
-		dogs[1] = new Dog();
-		dogs[1].size = 40;
-		dogs[1].breed = "Labrador Retriever";
-		dogs[1].name = "Lilly";
-		dogs[2] = new Dog();
-		dogs[2].size = 10;
-		dogs[2].breed = "Chihuahua";
-		dogs[2].name = "Charlie";
-
-		int x = 0;
-		int biggestDog = 0;
-		while (x < dogs.length) {
-			dogs[x].bark();
-			if (dogs[x].size > dogs[biggestDog].size) {
-				biggestDog = x;
-			}
-			if (x == 2) {
-				System.out.println("Biggest dog: " + dogs[biggestDog].name + ", " + dogs[biggestDog].breed + ", " + dogs[biggestDog].size);
-			}
-			x = x + 1;
-		}
+		Dog dog = new Dog();
+		dog.setSize(50);
+		Dog dog2 = new Dog();
+		dog2.setSize(60);
+		System.out.println(dog.isBigger(dog2));
 
 	}
 }
+
+// class DogTestDrive {
+// 	public static void main (String[] args) {
+
+// 		Dog[] dogs;
+// 		dogs = new Dog[3];
+// 		dogs[0] = new Dog();
+// 		dogs[0].size = 60;
+// 		dogs[0].breed = "Great Dane";
+// 		dogs[0].name = "Dan";
+// 		dogs[1] = new Dog();
+// 		dogs[1].size = 40;
+// 		dogs[1].breed = "Labrador Retriever";
+// 		dogs[1].name = "Lilly";
+// 		dogs[2] = new Dog();
+// 		dogs[2].size = 10;
+// 		dogs[2].breed = "Chihuahua";
+// 		dogs[2].name = "Charlie";
+
+// 		int x = 0;
+// 		int biggestDog = 0;
+// 		while (x < dogs.length) {
+// 			dogs[x].bark();
+// 			if (dogs[x].size > dogs[biggestDog].size) {
+// 				biggestDog = x;
+// 			}
+// 			if (x == 2) {
+// 				System.out.println("Biggest dog: " + dogs[biggestDog].name + ", " + dogs[biggestDog].breed + ", " + dogs[biggestDog].size);
+// 			}
+// 			x = x + 1;
+// 		}
+
+// 	}
+// }
